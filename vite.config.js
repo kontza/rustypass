37bb5c4e2429469fc592a15b9e06c3bb67a875af
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2023-06-26 16:25:29 +0300

    Vue front replaced with Svelte front

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  clearScreen: false,
   server: {
-    port: 7110
+    strictPort: true,
+    port: 9110
   },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG
+  },
+  plugins: [svelte()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
